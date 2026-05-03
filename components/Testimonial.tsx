c7f747e54cc3359312f054699fd61437e1fb242b
Didier Pereira <krrattoss5@gmail.com>
2026-05-02 20:45:11 -0500

    refactor: format testimonial component

--- components/Testimonial.tsx
+++ components/Testimonial.tsx
@@ -1,15 +1,17 @@
-import React from 'react'
-import TestimonialSlider from './TestimonialSlider'
-import { useTranslation } from 'next-i18next'
+import { useTranslation } from "next-i18next"
+import TestimonialSlider from "./TestimonialSlider"
 
 const Testimonial = () => {
-  const { t } = useTranslation('common')
+  const { t } = useTranslation("common")
   return (
-    <div className='bg-[#02050a] pt-[4rem] md:pt-[8rem]'>
-      <h1 className='heading'>
-        {t('testimonial.heading')} <span className='text-yellow-400'>{t('testimonial.headingHighlight')}</span>
+    <div className="bg-[#02050a] pt-[4rem] md:pt-[8rem]">
+      <h1 className="heading">
+        {t("testimonial.heading")}{" "}
+        <span className="text-yellow-400">
+          {t("testimonial.headingHighlight")}
+        </span>
       </h1>
-      <div className='pt-[5rem] pb-[4rem] w-[80%] mx-auto'>
+      <div className="pt-[5rem] pb-[4rem] w-[80%] mx-auto">
         <TestimonialSlider />
       </div>
     </div>
